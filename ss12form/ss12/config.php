dfde85ffe9d583fa5bb9f8946026fad5f6934273
nghenehihi12 <n24dvcn019@student.ptithcm.edu.vn>
2025-04-16 11:25:53 +0700

    Add files via upload

---
+++ ss12form/ss12/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'base' => '/ptit24cntt01/ss12form/ss12/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
